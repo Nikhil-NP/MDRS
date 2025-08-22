2fbc4de04710e529d28ea6a8b8ea471a329bdc9f
nikhil <nikhilrajpurohitnkp@gmail.com>
2025-08-23 01:06:53 +0530

    the maxDistancePerVehical is dynamic

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,24 @@ const { error } = require('console');
 const { stdout, stderr } = require('process');
 
 
-const numberOfCordinates = 25
-const numberOfVehicles = 5
-const maxDistancePerVehicle = 5000; //6km for now : needs to be more dynamic like distance based for devices
-const range = 0.05 //currently 5km range
+function calculateOptimalParameters(numberOfCordinates,numberOfVehicles,range){
+    const areaRadius = range * 111000 //approx km conversion ex: 0.05 ~~ 5.5km
+    const deliveriesPerVehicle = Math.ceil(numberOfCordinates/numberOfVehicles);
 
+    //calc feasible max distance
+    const maxDistancePerVehicle = Math.max(4000,
+        Math.min(15000,(areaRadius/numberOfVehicles) * deliveriesPerVehicle * 2)
+    
+    );
+    return { maxDistancePerVehicle };
+
+}
+
+
+const numberOfCordinates = 20
+const numberOfVehicles = 3
+const range = 0.05 //currently 5km range
+const {maxDistancePerVehicle} = calculateOptimalParameters(numberOfCordinates,numberOfVehicles,range); //6km for now : needs to be more dynamic like distance based for devices
 
 //a location in pune
 const fixed_location = {lat:18.49476,lng:73.890154};
@@ -18,9 +31,12 @@ function randomCordinateGenerator(numberOfCordinates){
     let cords = [fixed_location]; //for now only the fixed location in the res
 
     for (let i = 0; i < numberOfCordinates; i++) {
-        //basic fn that will find random location around a specific point
-        let lat = fixed_location.lat + Math.random() * range; // 
-        let lng = fixed_location.lng + Math.random() * range;
+        
+        let angle = Math.random() * 2 * Math.PI; //0 to 360 radious
+        let distance = Math.random() * range;
+
+        let lat = fixed_location.lat + distance * Math.cos(angle); // 
+        let lng = fixed_location.lng + distance * Math.sin(angle);
         cords.push({lat,lng});  
     }
     return cords
